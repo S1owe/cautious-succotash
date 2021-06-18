3495577c2fd5ea15997b353a49c5877492567a0b
S1owe <proaziz232@gmail.com>
2021-06-19 00:03:07 +0300

    Commite update v1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 
-let counter = 5;
+let counter = 2;
 
 function timer () {
     counter++;
+    console.log('counter = ' + counter);
     document.getElementById("count").innerHTML = counter;
     setTimeout(timer, 1000)
 }
