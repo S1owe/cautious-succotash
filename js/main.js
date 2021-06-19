bb50cafd3ecea2082c4e997ed32ffa50132d4530
S1owe <proaziz232@gmail.com>
2021-06-19 21:23:49 +0300

    Fix any v2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 console.log('This is fixBug');
 
-let count = counting_2(5);
+let count = counting_2(6);
 
 function counting_2(n) {
     return ((2^n) + (2^n))
 }
 
 console.log(count);
+
+
 
 
 
