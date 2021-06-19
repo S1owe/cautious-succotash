0bd3e246c438d9a3c466d92c757762be7c194138
S1owe <proaziz232@gmail.com>
2021-06-19 16:58:12 +0300

    New Commit 21

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 
 let counter = 5;
 
-alert('This is version 3');
+alert('This is main');
 
 
 function timer () {
     counter++;
+
+    if (counter === 100 ) {
+        console.log('This is 100');
+        return 0;
+    }
+
     document.getElementById("count").innerHTML = counter;
     setTimeout(timer, 1000)
 }
 
-//
 
