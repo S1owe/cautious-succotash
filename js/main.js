f3fa548a8f9d4a03a62c823f43450ccaf7ead789
S1owe <proaziz232@gmail.com>
2021-06-19 21:10:45 +0300

    Add function

--- js/main.js
+++ js/main.js
@@ -1,27 +1,15 @@
+console.log('This is fixBug');
 
-let counter = 0;
-let check = null;
+let count = counting_2(5);
 
-alert('This is fixBug');
-
-
-function timer () {
-    counter++;
-
-    if (counter === 3) {
-        check = true;
-        console.log(check);
-    }
-
-
-    if (counter === 100 ) {
-        console.log('This is 100');
-        return 0;
-    }
-
-    document.getElementById("count").innerHTML = counter;
-    setTimeout(timer, 1000)
+function counting_2(n) {
+    return ((2^n) + (2^n))
 }
+
+console.log(count);
+
+
+
 
 
 
