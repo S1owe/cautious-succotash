a9deaf76b5c13bbec2abf9bcc92e1c4ae289b25e
S1owe <proaziz232@gmail.com>
2021-06-19 16:44:29 +0300

    Commit update timer

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ alert('This is newBranch');
 
 function timer () {
     counter++;
+
+    if (counter === 10) {
+        alert('The end!');
+        return 0;
+    }
+
     document.getElementById("count").innerHTML = counter;
     setTimeout(timer, 1000)
 }
